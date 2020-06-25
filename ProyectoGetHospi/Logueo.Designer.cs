0de260a5610f606a3daa524e363cf255034f8631
Dayana Ivette <63428525+dayanaivette@users.noreply.github.com>
2020-06-25 11:48:11 -0600

    segundos diseños

--- ProyectoGetHospi/Logueo.Designer.cs
+++ ProyectoGetHospi/Logueo.Designer.cs
@@ -126,7 +126,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(231)))), ((int)(((byte)(159)))));
-            this.ClientSize = new System.Drawing.Size(327, 507);
+            this.ClientSize = new System.Drawing.Size(327, 495);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnEntrar);
--- ProyectoGetHospi/Vista/EliminarU.Designer.cs
+++ ProyectoGetHospi/Vista/EliminarU.Designer.cs
@@ -29,53 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EliminarU));
             this.dgvusuariosE = new System.Windows.Forms.DataGridView();
+            this.IdUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.usuariosBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.btnEliminar = new System.Windows.Forms.Button();
-            this.IdUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dUIDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nITDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.usuarioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.fechaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvusuariosE)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usuariosBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvusuariosE
             // 
-            this.dgvusuariosE.AllowUserToAddRows = false;
             this.dgvusuariosE.AllowUserToDeleteRows = false;
             this.dgvusuariosE.AutoGenerateColumns = false;
             this.dgvusuariosE.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvusuariosE.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(249)))), ((int)(((byte)(231)))));
+            this.dgvusuariosE.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvusuariosE.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(208)))), ((int)(((byte)(63)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(149)))), ((int)(((byte)(11)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvusuariosE.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvusuariosE.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvusuariosE.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.IdUsuario,
-            this.nombreDataGridViewTextBoxColumn,
-            this.dUIDataGridViewTextBoxColumn,
-            this.nITDataGridViewTextBoxColumn,
-            this.usuarioDataGridViewTextBoxColumn,
-            this.fechaDataGridViewTextBoxColumn});
+            this.IdUsuario});
             this.dgvusuariosE.DataSource = this.usuariosBindingSource;
-            this.dgvusuariosE.Location = new System.Drawing.Point(13, 60);
+            this.dgvusuariosE.EnableHeadersVisualStyles = false;
+            this.dgvusuariosE.Location = new System.Drawing.Point(33, 47);
             this.dgvusuariosE.Name = "dgvusuariosE";
             this.dgvusuariosE.ReadOnly = true;
-            this.dgvusuariosE.Size = new System.Drawing.Size(634, 200);
+            this.dgvusuariosE.Size = new System.Drawing.Size(547, 209);
             this.dgvusuariosE.TabIndex = 0;
-            // 
-            // usuariosBindingSource
-            // 
-            //this.usuariosBindingSource.DataSource = typeof(ProyectoGetHospi.Entidades.Usuarios);
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(13, 31);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(153, 23);
-            this.btnEliminar.TabIndex = 1;
-            this.btnEliminar.Text = "Eliminar Seleccionado";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // IdUsuario
             // 
@@ -84,48 +73,32 @@
             this.IdUsuario.Name = "IdUsuario";
             this.IdUsuario.ReadOnly = true;
             // 
-            // nombreDataGridViewTextBoxColumn
+            // btnEliminar
             // 
-            this.nombreDataGridViewTextBoxColumn.DataPropertyName = "Nombre";
-            this.nombreDataGridViewTextBoxColumn.HeaderText = "Nombre";
-            this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
-            this.nombreDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // dUIDataGridViewTextBoxColumn
-            // 
-            this.dUIDataGridViewTextBoxColumn.DataPropertyName = "DUI";
-            this.dUIDataGridViewTextBoxColumn.HeaderText = "DUI";
-            this.dUIDataGridViewTextBoxColumn.Name = "dUIDataGridViewTextBoxColumn";
-            this.dUIDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // nITDataGridViewTextBoxColumn
-            // 
-            this.nITDataGridViewTextBoxColumn.DataPropertyName = "NIT";
-            this.nITDataGridViewTextBoxColumn.HeaderText = "NIT";
-            this.nITDataGridViewTextBoxColumn.Name = "nITDataGridViewTextBoxColumn";
-            this.nITDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // usuarioDataGridViewTextBoxColumn
-            // 
-            this.usuarioDataGridViewTextBoxColumn.DataPropertyName = "Usuario";
-            this.usuarioDataGridViewTextBoxColumn.HeaderText = "Usuario";
-            this.usuarioDataGridViewTextBoxColumn.Name = "usuarioDataGridViewTextBoxColumn";
-            this.usuarioDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // fechaDataGridViewTextBoxColumn
-            // 
-            this.fechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha";
-            this.fechaDataGridViewTextBoxColumn.HeaderText = "Fecha";
-            this.fechaDataGridViewTextBoxColumn.Name = "fechaDataGridViewTextBoxColumn";
-            this.fechaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(208)))), ((int)(((byte)(63)))));
+            this.btnEliminar.FlatAppearance.BorderSize = 0;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(397, 274);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(183, 34);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar Seleccionado";
+            this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // EliminarU
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 276);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(231)))), ((int)(((byte)(159)))));
+            this.ClientSize = new System.Drawing.Size(607, 327);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.dgvusuariosE);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "EliminarU";
             this.Text = "Eliminar Usuario";
             this.Load += new System.EventHandler(this.eliminarUsuario_Load);
